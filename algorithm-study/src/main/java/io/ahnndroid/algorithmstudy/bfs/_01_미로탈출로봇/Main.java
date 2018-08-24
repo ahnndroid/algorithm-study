ee8ae65f66cf7d1707c0d3e071cacec768a0ecdb
DESKTOP-2Q1MURN\ahnndroid <super702@gmail.com>
2018-08-24 20:51:39 +0900

    [BFS] 장기 문제풀이 업데이트

--- algorithm-study/src/main/java/io/ahnndroid/algorithmstudy/bfs/_01_미로탈출로봇/Main.java
+++ algorithm-study/src/main/java/io/ahnndroid/algorithmstudy/bfs/_01_미로탈출로봇/Main.java
@@ -8,15 +8,18 @@ import java.util.Scanner;
 미로탈출 로봇 대회를 개최하였다. 
 대회에 사용되는 미로는 가로(WIDTH), 세로(HEIGHT) 100이하의 크기이며, 미로를 한 칸 이동하는 데는 1초가 걸린다.
 로봇이 출발점에서 도착점까지 가장 빨리 이동할 경우 걸리는 시간을 구하는 프로그램을 작성하시오.
+
  
 [ 입력 ]
 첫 줄에 미로의 크기 X, Y(1≤X, Y≤100)가 주어진다.
 둘째 줄에 출발점 x, y 좌표와 도착점 x, y 좌표가 공백으로 구분하여 주어진다.
 셋째 줄부터 미로의 정보가 길은 0, 벽은 1로 공백이 없이 들어온다.
 주의 사항으로, 좌표는 좌측상단이 가장 작은 위치이며 이 위치의 좌표는 (1,1)이다.
+
  
 [ 출력 ]
 첫 줄에 출발점에서 도착점까지 가장 빠른 시간을 출력한다.
+ 
  
 [ 입력 예시 ]
 8 7
@@ -29,6 +32,7 @@ import java.util.Scanner;
 10000001
 11111111
  
+ 
 [ 출력 예시 ]
 9
 	
@@ -37,45 +41,69 @@ import java.util.Scanner;
  */
 public class Main {
 	
-	// 미로의 행렬 사이즈를 입력 받는 전역 변수 (X는 세로, Y는 가로)
+	// 미로의 행렬 사이즈를 입력 받는 전역 변수 (X는 세로(= 열), Y는 가로(= 행))
 	static int X, Y;
+	
 	// 출발점 좌표를 입력 받는 전역 변수
 	static int SX, SY;
+	
 	// 도착점 좌표를 입력 받는 전역 변수
 	static int EX, EY;
 	
 	// 미로판 배열 전역 변수
-	static char[][] miro = new char[Y+10][X+10];
-	// 미로 방문 여부 저장을 위한 배열 전역 변수
-	static int[][] visit = new int[Y+10][X+10];
+	static char[][] miro;
 	
-	// 큐 원소에 관한 내부 클래스
-	static class Element {
-		int currY;		// 원소의 행 좌표 위치
-		int currX;		// 원소의 열 좌표 위치
-		int v;			// 원소 방문 여부 플래그
-		
-		Element(int y, int x, int v) {
-			this.currY = y;
-			this.currX = x;
-			this.v = v;
-		}
-	}
+	// 미로 방문 여부 저장을 위한 배열 전역 변수
+	static int[][] visit;
+	
 	// 큐 전역 변수
-	static Element[] Queue = new Element[(Y+10) * (X+10)];
+	static Element[] Queue;
+	
 	// 큐의 쓰기 & 읽기 위치를 저장하는 포인터 관련 전역 변수
 	static int wp, rp;
-	// 큐에서 꺼낸 원소 기준 상하좌우 탐색을 위한 오프셋 배열 전역 변수 선언 
+	
+	// 큐에서 꺼낸 원소 기준 상하좌우 탐색용 오프셋 배열 전역 변수 선언 
 	static int[] offsetY = { -1, 1, 0, 0 };
 	static int[] offsetX = { 0, 0, -1, 1 };
 	
 	// 이동 시간 누적합(정답) 출력용 전역 변수
-	static int sol = 0;
+	static int sol;
+	
+	
+	/**
+	 * 초기화 작업 진행
+	 */
+	static void init() {
+		initStaticVariables();
+		initInputValues();
+	}
+	
+	/**
+	 * 전역 변수 초기화
+	 */
+	static void initStaticVariables() {
+		// 미로판 배열 초기화
+		miro = new char[100 + 10][100 + 10];
+		
+		// 미로 방문 여부 마킹 배열 초기화
+		visit = new int[100 + 10][100 + 10];
+		
+		// 큐 초기화
+		Queue = new Element[(100 + 10) * (100 + 10)];
+		
+		// 큐의 쓰기 & 읽기 위치 저장 포인터 변수 초기화
+		wp = 0;
+		rp = 0;
+		
+		// 이동 시간 누적합 출력용 변수 초기화
+		sol = 0;
+	}
 	
 	/**
 	 * 알고리즘 연산에 필요한 전역 변수 초기화 작업을 진행하는 유틸 메소드
 	 */
-	static void init() {
+	static void initInputValues() {		
+		
 		Scanner sc = new Scanner(System.in);
 		
 		// 미로 행렬 사이즈 입력
@@ -125,6 +153,7 @@ public class Main {
 				currY = out.currY + offsetY[k];
 				currX = out.currX + offsetX[k];
 				
+				
 				// Step 4) 탐색한 현재 좌표의 원소가...
 				// Step 4-1) 미로판 범위를 벗어난 상태면 continue
 				if (currY < 1 || currY > Y || currX < 1 || currX > X) continue;
@@ -138,6 +167,7 @@ public class Main {
 				// Step 4-4) 목적지와 일치하면 결과 리턴
 				if (currY == EY && currX == EX)	return out.v + 1;
 				
+
 				// Step 5) 목적지에 일단 도달하지 않은 좌표의 원소인 경우...
 				// Step 5-1) 방문했다는 마킹을 하고
 				visit[currY][currX] = 1;
@@ -159,6 +189,13 @@ public class Main {
 	}
 	
 	/**
+	 * 결과 출력
+	 */
+	static void print() {
+		System.out.println(sol);
+	}
+	
+	/**
 	 * 큐에서 원소를 꺼낼 시 이용하는 유틸 메소드
 	 * @return
 	 */
@@ -168,12 +205,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		init();			// 입력 초기화 작업 진행
+		init();			// 초기화 작업 진행
 		
 		sol = BFS();	// 가장 빠른 이동 시간 계산을 위한 너비 우선 탐색 진행
 		
-		System.out.println(sol);
+		print();		// 결과 출력
 		
 	}
 
+	
+	
+	/**
+	 * 큐 원소 관련 내부 클래스
+	 * 
+	 * @author ahnndroid
+	 *
+	 */
+	static class Element {
+		int currY;		// 원소의 행 좌표 위치
+		int currX;		// 원소의 열 좌표 위치
+		int v;			// 원소 방문 여부 플래그
+		
+		Element(int y, int x, int v) {
+			this.currY = y;
+			this.currX = x;
+			this.v = v;
+		}
+	}
 }
--- algorithm-study/src/main/java/io/ahnndroid/algorithmstudy/bfs/_02_장기/Main.java
+++ algorithm-study/src/main/java/io/ahnndroid/algorithmstudy/bfs/_02_장기/Main.java
@@ -1,5 +1,7 @@
 package io.ahnndroid.algorithmstudy.bfs._02_장기;
 
+import java.util.Scanner;
+
 /**
  * 
 [ 문제 설명 ]
@@ -27,10 +29,184 @@ N×M장기판에 졸 한 개와 말 한 개가 놓여 있다.
  *
  */
 public class Main {
+	
+	// 장기판 행(N)과 열(M)을 입력 받을 전역 변수 선언
+	static int N, M;
+	
+	// 말이 위치한 행(R)과 열(C)을 입력 받을 전역 변수 선언
+	static int R, C;
+	
+	// 졸이 위치한 행(S)과 열(K)을 입력 받을 전역 변수 선언
+	static int S, K;
+	
+	// 장기판 배열 전역 변수 선언
+	static int[][] jangi;
+	
+	// 큐 객체 전역 변수 선언
+	static Element[] Queue;
+	
+	// 큐의 추출 위치 포인터(dp: dequeuePointer) 관련 전역 변수 선언
+	static int dp;
+	
+	// 큐의 삽입 위치 포인터(ep: enqueuePointer) 관련 전역 변수 선언
+	static int ep;
+	
+	// 최소 이동 횟수 관련 결과값 출력을 위한 전역 변수 선언
+	static int sol;
+	
+	
+	/**
+	 * 초기화 작업 진행 관련 유틸 메소드
+	 */
+	static void init() {
+		// 전역 변수 초기값 설정
+		initStaticVariables();
+		
+		// 사용자 입력 초기화
+		initInputVariables();
+	}
+	
+	/**
+	 * 전역 변수 초기값 설정
+	 */
+	static void initStaticVariables() {
+		jangi = new int[100 + 10][100 + 10];
+		
+		Queue = new Element[(100 + 10) * (100 + 10)];
+		
+		dp = 0;
+		ep = 0;
+		
+		sol = 0;
+	}
+	
+	/**
+	 * 사용자 입력 관련 전역 변수 초기화
+	 */
+	static void initInputVariables() {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		R = sc.nextInt();
+		C = sc.nextInt();
+		
+		S = sc.nextInt();
+		K = sc.nextInt();
+		
+		sc.close();
+	}
+	
+	
+	/**
+	 * 최소 이동 횟수를 찾기 위한 너비 우선 탐색 진행
+	 * 
+	 * @return
+	 */
+	static int BFS() {
+		
+		// BFS 탐색 시 이용될 행렬 오프셋 배열 선언
+		int[] rowOffset = { -2, -1,  1,  2,  2,  1, -1, -2 };
+		int[] colOffset = { -1, -2, -2, -1,  1,  2,  2,  1 };
+		
+		// BFS 탐색 대상 원소의 행렬 좌표 저장 변수
+		int currRow, currCol;
+		
+		// Step 1) 시작 원소의 큐 삽입 & 해당 원소의 좌표를 방문했음을 마킹
+		enQueue(R, C, 0);
+		jangi[R][C] = 1;
+		
+		// Step 2) 큐에 원소가 존재하는 동안 루프를 반복하면서 (= enqueuePointer가 dequeuePointer보다 큰 동안)
+		while (ep > dp) {
+			
+			// Step 3) 큐에서 원소를 하나 빼내서
+			Element out = deQueue();
+			
+			// Step 4) 빼낸 원소 좌표를 기준으로 상하좌우대각선 탐색 과정 진행
+			for (int i = 0; i < 8; i++) {
+				
+				// Step 4-1) 탐색 대상 원소의 좌표 추출
+				currRow = out.row + rowOffset[i];
+				currCol = out.col + colOffset[i];
+				
+				// Step 4-2) 탐색 대상 원소가 범위를 벗어난 경우 continue
+				if ( currRow < 1 || currRow > N || currCol < 1 || currCol > M )	continue;
+				
+				// Step 4-3) 탐색 대상 원소가 방문했던 곳이면 continue
+				if ( jangi[currCol][currRow] == 1 )	continue;
+				
+				// Step 4-4) 탐색 대상 원소가 졸의 위치와 동일하면 이동 회수를 1 증가 후 return
+				if ( currRow == S && currCol == K )	return out.v + 1;
+				
+				// Step 5) 아직 졸을 만나지 못했다면
+				// Step 5-1) 다음 탐색을 위해 현재 탐색 대상 원소를 큐에 삽입하고
+				enQueue(currRow, currCol, out.v + 1);
+				
+				// Step 5-2) 해당 탐색 대상 원소의 좌표는 방문했었음을 마킹
+				jangi[currCol][currRow] = 1;
+			}
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * 큐 원소 삽입 유틸 메소드
+	 * 
+	 * @param row
+	 * @param col
+	 * @param v
+	 */
+	static void enQueue(int row, int col, int v) {
+		Queue[ep++] = new Element(row, col, v);
+	}
+	
+	/**
+	 * 큐 원소 빼내기 유틸 메소드
+	 * @return
+	 */
+	static Element deQueue() {
+		return Queue[dp++];
+	}
+	
+	/**
+	 * 결과 출력
+	 */
+	static void print() {
+		System.out.println(sol);
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// 초기화 작업 진행
+		init();
+		
+		// 장기 말이 졸을 찾는데 이동한 최소 이동 횟수를 찾기 위한 너비 우선 탐색 진행
+		sol = BFS();
+		
+		// 결과 출력
+		print();
+	}
+	
+	
+	/**
+	 * 큐 원소 관련 내부 클래스
+	 * 
+	 * @author ahnndroid
+	 *
+	 */
+	static class Element {
+		int row;	// 큐 원소의 행 위치 
+		int col;	// 큐 원소의 열 위치
+		int v;		// 큐 원소의 이동 횟수
+		
+		Element(int row, int col, int v) {
+			this.row = row;
+			this.col = col;
+			this.v = v;
+		}
 	}
 
 }
